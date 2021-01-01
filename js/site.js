1be991597c3e5b791108b5b83bf7348036fe97a3
Daniel Otulagun <dotulagun@seamfix.com>
2021-01-01 18:07:59 +0100

    adding portfolio code

---
+++ js/site.js
@@ -0,0 +1,17 @@
+﻿const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+    document.body.classList.toggle('static-body');
+    document.querySelector('.hamburger').classList.toggle('hamToggle');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+        document.body.classList.remove('static-body');
+        document.querySelector('.hamburger').classList.remove('hamToggle');
+    })
+})
